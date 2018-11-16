672e060004e557fcbd3b1a99e04e33b0f39cc782
Amam Mustofa <mustofa@binary.com>
2018-11-16 09:54:09 +0800

    fix responsiveness issue

--- index.js
+++
@@ -1,6 +0,0 @@
-
-var header = document.getElementById('opacitor');
-window.addEventListener('scroll', function(e){
-	var scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 1;
-    header.style.opacity = Math.min(0, Math.max(1, -scroll));
-});
